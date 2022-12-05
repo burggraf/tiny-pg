a62dd84490ffa890bc2ccdda3fca3ef168880ae7
Mark Burggraf <markb@mantisbible.com>
2022-12-05 07:16:16 -0800

    add name to running container

--- alpine-postgres.sh
+++ alpine-postgres.sh
@@ -2,7 +2,7 @@
 if [ -n "$1" ] && [ "$1" -eq "$1" ] 2>/dev/null; then
   echo Starting PostgreSQL instance on port: $1
   docker volume create alpine-postgres-data-$1
-  docker run -v alpine-postgres-data-$1:/var/lib/postgresql/data -p $1:5432 -it burggraf/alpine-postgres
+  docker run --name alpine-postgres-$1 -v alpine-postgres-data-$1:/var/lib/postgresql/data -p $1:5432 -it burggraf/alpine-postgres
 else
   echo "You must supply a port number as the first argument."
   echo "Example: ./start 5432"
