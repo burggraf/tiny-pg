f7bf6ec702dfe5a96dfc13c7c8c81a68ad1da1c1
Mark Burggraf <markb@mantisbible.com>
2022-12-08 06:50:10 -0800

    fixed startup process

--- start-postgres.sh
+++ start-postgres.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
-su postgres -c 'initdb -D /var/lib/postgresql/data'
-echo "host all all 0.0.0.0/0 md5" >> /var/lib/postgresql/data/pg_hba.conf && \
-echo "listen_addresses='*'" >> /var/lib/postgresql/data/postgresql.conf && \
-su postgres -c 'pg_ctl start -D /var/lib/postgresql/data'
-psql -U postgres -c "alter role postgres with password 'postgres'"
-psql -U postgres && /bin/sh
+FILE=/var/lib/postgresql/data/postgresql.conf
+if [ -f "$FILE" ]; then
+    # postgresql is already initialized, just start it
+    su postgres -c 'pg_ctl start -D /var/lib/postgresql/data'
+    psql -U postgres && /bin/sh
+else 
+    # postgresql is not initialized, initialize it
+    su postgres -c 'initdb -D /var/lib/postgresql/data'
+    # set up access to the outside world
+    echo "host all all 0.0.0.0/0 md5" >> /var/lib/postgresql/data/pg_hba.conf && \
+    echo "listen_addresses='*'" >> /var/lib/postgresql/data/postgresql.conf && \
+    # start postgresql
+    su postgres -c 'pg_ctl start -D /var/lib/postgresql/data'
+    # set the default password for the postgres user
+    psql -U postgres -c "alter role postgres with password 'postgres'"
+    # launch psql
+    psql -U postgres && /bin/sh
+fi
