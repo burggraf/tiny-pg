807162647b6b5110a746928f3c6e6e54546791bb
Mark Burggraf <markb@mantisbible.com>
2022-12-23 07:25:00 -0800

    cleanup

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -191,7 +191,6 @@
 						</ion-col>
 						<ion-col size="2" class="ion-text-center">					
 							{#if extension.installed}
-								<!-- {#if extension.enabled} -->
 								<ion-toggle color="primary" 
 								on:ionChange={() => {
 									if (extension.enabled) 
@@ -200,19 +199,10 @@
 									run_script(`/cgi-bin/enable_extension.sh?${extension.name}`,`Enabling extension ${extension.name}...`)
 								}}
 								checked={extension.enabled}></ion-toggle>
-									<!-- <ion-button 
-									on:click={() => {run_script(`/cgi-bin/disable_extension.sh?${extension.name}`,`Disabling extension ${extension.name}...`)}} 
-									size="small" fill="outline">Disable</ion-button>
-								{:else}
-									<ion-button 
-									on:click={() => {run_script(`/cgi-bin/enable_extension.sh?${extension.name}`,`Enabling extension ${extension.name}...`)}} 
-									size="small" fill="outline">Enable</ion-button>
-								{/if} -->
 							{:else}
 								&nbsp;
 							{/if}
 						</ion-col>
-						<!-- <ion-col>{extension.size}</ion-col> -->
 					</ion-row>
 					<ion-row>
 						<ion-col size="12">&nbsp;</ion-col>
@@ -222,114 +212,7 @@
 		</ion-grid>
 	{/if}
 
-	<!-- <ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql15-contrib-jit')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgresql15-contrib-jit',"Installing postgresql15-contrib-jit...")}}>
-	postgresql15-contrib-jit</ion-button>
-
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgis')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgis',"Installing postgis...")}}>
-	postgis</ion-button>
-
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql15-plperl-contrib')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgresql15-plperl-contrib',"Installing postgresql15-plperl-contrib...")}}>
-	postgresql15-plperl-contrib</ion-button>
-	
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql15-pltcl')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgresql15-pltcl',"Installing postgresql15-pltcl...")}}>
-	postgresql15-pltcl</ion-button>
-
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql15-plpython3')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgresql15-plpython3',"Installing postgresql15-plpython3...")}}>
-	postgresql15-plpython3</ion-button>
-
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql15-plpython3-contrib')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgresql15-plpython3-contrib',"Installing postgresql15-plpython3-contrib...")}}>
-	postgresql15-plpython3-contrib</ion-button>
-
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql-mysql_fdw')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgresql-mysql_fdw',"Installing postgresql-mysql_fdw...")}}>
-	postgresql-mysql_fdw</ion-button>
-
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql-temporal_tables')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgresql-temporal_tables',"Installing postgresql-temporal_tables...")}}>
-	postgresql-temporal_tables</ion-button>
-
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql-plpgsql_check')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgresql-plpgsql_check',"Installing postgresql-plpgsql_check...")}}>
-	postgresql-plpgsql_check</ion-button>
-
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql-url_encode')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgresql-url_encode',"Installing postgresql-url_encode...")}}>
-	postgresql-url_encode</ion-button>
-
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql-uint')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgresql-uint',"Installing postgresql-uint...")}}>
-	postgresql-uint</ion-button>
-
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql-orafce')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgresql-orafce',"Installing postgresql-orafce...")}}>
-	postgresql-orafce</ion-button>
-
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql-rum')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgresql-rum',"Installing postgresql-rum...")}}>
-	postgresql-rum</ion-button>
-
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql-bdr-extension')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgresql-bdr-extension',"Installing postgresql-bdr-extension...")}}>
-	postgresql-bdr-extension</ion-button>
-
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql-hypopg')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgresql-hypopg',"Installing postgresql-hypopg...")}}>
-	postgresql-hypopg</ion-button>
-
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql-pllua')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgresql-pllua',"Installing postgresql-pllua...")}}>
-	postgresql-pllua</ion-button>
-
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql-sequential-uuids')}
-	on:click={() => {run_script('/cgi-bin/add.sh?postgresql-sequential-uuids',"Installing postgresql-sequential-uuids...")}}>
-	postgresql-sequential-uuids</ion-button>
-
-	<ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql-citus-bitcode')}
-	on:click={() => {run_script('/cgi-bin/citus.sh',"Installing citus...")}}>
-	citus**</ion-button> -->
-
-	<!-- <ion-button expand="block" size="small" fill="outline" 
-	on:click={() => {run_script('/cgi-bin/shared_ispell.sh',"Installing shared_ispell...")}}>
-	shared_ispell**</ion-button> -->
-
-	<!-- <ion-button expand="block" size="small" fill="outline" 
-	disabled={installed_packages.includes('postgresql-pg_cron')}
-	on:click={() => {run_script('/cgi-bin/pg_cron.sh',"Installing pg_cron...")}}>
-	pg_cron**</ion-button> -->
-
-	<br/>** = requires PostgreSQL restart<br/>
-
 	<pre id="output"></pre>
-
-	Installed: 
-	<div>{JSON.stringify(installed_packages)}</div>
-	<div>Total size: {total_size.toLocaleString()} bytes</div>
-	<div>Extensions: {JSON.stringify(extensions)}</div>
-	<div>Installed Extensions: {JSON.stringify(installed_extensions)}</div>
 	
 </ion-content>
 
