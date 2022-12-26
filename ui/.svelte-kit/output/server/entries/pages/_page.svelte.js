48cd27778c4db69bec79ef4e8c4bced1cb5af53d
Mark Burggraf <markb@mantisbible.com>
2022-12-26 05:57:33 -0800

    first deploy

--- ui/.svelte-kit/output/server/entries/pages/_page.svelte.js
+++ ui/.svelte-kit/output/server/entries/pages/_page.svelte.js
@@ -1,39 +1,26 @@
-import { c as create_ssr_component, e as escape } from "../../chunks/index.js";
+import { c as create_ssr_component, e as escape, d as add_attribute } from "../../chunks/index.js";
 import "@ionic/core";
 import "../../chunks/platform.js";
-import "ionicons/icons";
+import * as allIonicIcons from "ionicons/icons";
 const _page_svelte_svelte_type_style_lang = "";
 const css = {
-  code: ".grid375.svelte-tje6jk{max-width:375px}.header_col.svelte-tje6jk{font-weight:bold}.inputItemWithIcon.svelte-tje6jk{height:50px;border:1px solid rgb(212, 212, 212);background-color:var(--ion-background-color) !important;border-radius:5px}.inputIcon.svelte-tje6jk{padding-right:10px}.gridItem.svelte-tje6jk{--padding-start:0px;--padding-end:0px;--inner-padding-end:0px}",
+  code: ".grid375.svelte-1vmm8g4{max-width:375px}.header_col.svelte-1vmm8g4{font-weight:bold}.inputItemWithIcon.svelte-1vmm8g4{height:50px;border:1px solid rgb(212, 212, 212);background-color:var(--ion-background-color) !important;border-radius:5px}.inputIcon.svelte-1vmm8g4{padding-right:10px}.gridItem.svelte-1vmm8g4{--padding-start:0px;--padding-end:0px;--inner-padding-end:0px}",
   map: null
 };
 const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  let output = "";
+  let base = "";
   $$result.css.add(css);
   return `<ion-header><ion-toolbar translucent="${"true"}"><ion-buttons slot="${"start"}"><ion-menu-button></ion-menu-button></ion-buttons>
 		<ion-title>${escape(
     "Install PostgreSQL"
   )}</ion-title></ion-toolbar></ion-header>
-<ion-content class="${"ion-padding"}"><h3>PostgreSQL: ${escape("Not installed")}</h3>
+<ion-content class="${"ion-padding"}"><ion-grid class="${"ion-padding grid375 svelte-1vmm8g4"}"><ion-row><ion-col>Host URL</ion-col></ion-row>
+		<ion-row><ion-col><ion-input class="${"inputItemWithIcon svelte-1vmm8g4"}" type="${"text"}"${add_attribute("value", base, 0)} placeholder="${"URL of database server"}"><ion-icon class="${"inputIcon svelte-1vmm8g4"}"${add_attribute("icon", allIonicIcons.globeOutline, 0)} slot="${"start"}" size="${"large"}" color="${"medium"}"></ion-icon></ion-input></ion-col></ion-row>
+		<ion-row><ion-col><ion-button expand="${"block"}" size="${"large"}" fill="${"solid"}">Load Server</ion-button></ion-col></ion-row></ion-grid>
 
-	<ion-button>post_test</ion-button>
-	<br>
-
-	${`<ion-button expand="${"block"}" size="${"small"}" fill="${"outline"}">Install PostgreSQL 15</ion-button>
-
-		<ion-button expand="${"block"}" size="${"small"}" fill="${"outline"}">Install PostgreSQL 14</ion-button>
-
-		<ion-button expand="${"block"}" size="${"small"}" fill="${"outline"}">Install PostgreSQL 13</ion-button>
-
-		<ion-button expand="${"block"}" size="${"small"}" fill="${"outline"}">Install PostgreSQL 12</ion-button>`}
-
+	
 	${``}
-
-	${`<pre>${escape(output)}</pre>`}
-
-	${``}
-
-
+	
 </ion-content>`;
 });
 export {
--- ui/.svelte-kit/output/server/index.js
+++ ui/.svelte-kit/output/server/index.js
@@ -2412,7 +2412,7 @@ class Server {
       app_template,
       app_template_contains_nonce: false,
       error_template,
-      version: "1671983899139"
+      version: "1672063016541"
     };
   }
   async init({ env }) {
