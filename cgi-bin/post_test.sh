8153adf547353c911b83901236f33df535032cff
Mark Burggraf <markb@mantisbible.com>
2022-12-27 06:16:08 -0800

    remove post_test

--- cgi-bin/post_test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env /bin/sh
-printf "HTTP/1.0 200 OK\r\n"
-printf "Content-type: text/plain\r\n\r\n"
-
-RAW_DATA="$(cat)"
-DATA=$(echo -n "$RAW_DATA" | head -1 | tr -d '\r\n');
-echo "DATA: $DATA"
-
